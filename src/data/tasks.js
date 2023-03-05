9a4ebbb80547bf10faa24d1912a8909dc3e0df53
Alieski <alieskivelizvega@gmail.com>
2023-03-05 12:25:52 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id:0,
+        title:'Mi primer tarea',
+        description:'mi primer tarea descripcion'
+    },
+    {
+        id:1,
+        title:'Mi segunda tarea',
+        description:'mi segunda tarea descripcion'
+    },
+    {
+        id:2,
+        title:'Mi tercer tarea',
+        description:'mi tercer tarea descripcion'
+    }
+]
